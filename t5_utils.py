fd45329178899dc21ed97ead57c577fe1310c776
KirilIakovliev <31926158+KirilIakovliev@users.noreply.github.com>
2021-04-15 05:27:35 +0300

    Update t5_utils.py

--- t5_utils.py
+++ t5_utils.py
@@ -1,4 +1,5 @@
 import re
+import streamlit as st
 
 def clean_text(text):
     text = text.split()
@@ -8,6 +9,7 @@ def clean_text(text):
     text = re.sub('([.,!?()])', r' \1 ', text)
     return text
 
+@st.cache()
 def get_texts(df):
     texts = 'multilabel classification: ' + df['Text'].apply(clean_text)
     texts = texts.values.tolist()
